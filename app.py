9a50f2676223d23dfb075e5874b62fff35796fda
YZHAN295 <YZHAN295>
2017-10-21 22:49:42 +0800

    init index.html and ajax api
    init index.html and ajax api

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/query_vehicle_data')
+def query_vehicle_data():
+    return jsonify({'ok': True})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
